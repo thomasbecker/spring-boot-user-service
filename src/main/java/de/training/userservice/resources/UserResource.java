f93d33c62243b80b94580b6a625f5740a9af65d1
Thomas Becker <thomas.becker00@gmail.com>
2023-04-11 14:53:04 +0200

    add a real getUsers method and add User model class

--- src/main/java/de/training/userservice/resources/UserResource.java
+++ src/main/java/de/training/userservice/resources/UserResource.java
@@ -1,8 +1,11 @@
 package de.training.userservice.resources;
 
+import de.training.userservice.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
 
 /**
  * Created by Thomas Becker (thomas.becker00@gmail.com) on 11.04.23.
@@ -12,7 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource {
 
     @GetMapping
-    public String sayHello() {
-        return "Hello world!";
+    public Set<User> getUsers() {
+        return Set.of(
+                new User("Tom", "Jones", "tom.jones@mail.com"),
+                new User("Gunnar", "Wild", "gunnar.wild@somemail.com")
+        );
     }
 }
